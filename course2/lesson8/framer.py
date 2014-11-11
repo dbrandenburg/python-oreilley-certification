42b44ccff0558ebb7b5430322e5f4752cec62b34
Dennis Brandenburg <d.brandenburg@db-network.de>
2014-11-11 10:20:47 +0100

    Grif and Frames init

--- course2/lesson8/framer.py
+++ course2/lesson8/framer.py
@@ -8,35 +8,47 @@ class Application(Frame):
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.bind("<Button-1>", self.handler)
-        
-        self.master.rowconfigure(0, weight=1)
-        self.master.columnconfigure(0, weight=1)
+        #self.master.rowconfigure(0, weight=1)
+        #self.master.columnconfigure(0, weight=1)
         self.grid(sticky=ALL)
+        self.master.title("Framer")
         
-        self.rowconfigure(0, weight=1) 
-        Label(self, text="Frame {0}".format(1),bg="red").grid(row=0, column=0, sticky=ALL, columnspan=2)
-        self.rowconfigure(1, weight=1)
-        Label(self, text="Frame {0}".format(2),bg="green").grid(row=1, column=0, sticky=ALL, columnspan=2)
-        self.rowconfigure(5, weight=2)
-        Label(self, text="Frame {0}".format(3),bg="yellow").grid(row=0, column=2, sticky=ALL, rowspan=2, columnspan=3)
-        self.rowconfigure(5, weight=1)
-        
+        for r in range(6):
+             self.master.rowconfigure(r, weight=1)    
         for c in range(5):
-            self.columnconfigure(c, weight=1)
-            Button(self, text="Col {0}".format(c)).grid(row=2, column=c, sticky=ALL)
+            self.master.columnconfigure(c, weight=1)
+            Button(master, text="Button {0}".format(c)).grid(row=6,column=c,sticky=E+W)
             
-    def handler(event):
-        print("clicked at", event.x, event.y)    
+#        Frame1 = Frame(master, bg="red")
+#        Frame1.grid(row = 0, column = 0, rowspan = 3, columnspan = 2, sticky = ALL)
+#        Frame2 = Frame(master, bg="green")
+#        Frame2.grid(row = 3, column = 0, rowspan = 3, columnspan = 2, sticky = ALL)
+#        Frame3 = Frame(master, bg="blue")
+#        Frame3.grid(row = 0, column = 2, rowspan = 6, columnspan = 3, sticky = ALL)
+        
+        
+        top_frame = Frame(master, bg="red")
+        top_frame.grid(row=0, column=0, rowspan=3, columnspan=2, sticky=ALL)
+        #top_frame.rowconfigure(0, weight=1)
+        l = Label(top_frame, text="Frame {0}".format(1),bg="red")
+        l.grid(row=0, column=0, rowspan=3, columnspan=2, sticky=ALL)
+#        
+#        bottom_frame = Frame(self)
+#        bottom_frame.grid(sticky=ALL)
+#        bottom_frame.rowconfigure(1, weight=1)
+#        Label(bottom_frame, text="Frame {0}".format(2),bg="green").grid(row=1, column=0, sticky=ALL, columnspan=2)
+#        
+#        main_frame = Frame(self)
+#        main_frame.grid(sticky=ALL)
+#        main_frame.rowconfigure(5, weight=2)
+#        Label(main_frame, text="Frame {0}".format(3),bg="yellow").grid(row=0, column=2, sticky=ALL, rowspan=2, columnspan=3)
+        
+#        self.rowconfigure(5, weight=1)
+        
+#        for c in range(5):
+#            self.columnconfigure(c, weight=1)
+#            Button(self, text="Col {0}".format(c)).grid(row=2, column=c, sticky=ALL)
 
 root = Tk()
-app = Application(master=root)   
-app.pack()             
+app = Application(master=root)                
 app.mainloop()
-
-
-
-#root = Tk()
-#def handler(event):
-#print("clicked at", event.x, event.y)
-#frame = Frame(root, width=100, height=100) frame.bind("<Button-1>", handler) frame.pack()
--- course2/lesson9/framer.py
+++ course2/lesson9/framer.py
@@ -8,21 +8,43 @@ class Application(Frame):
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
+ 
+        
         self.master.rowconfigure(0, weight=1)
         self.master.columnconfigure(0, weight=1)
-        self.grid(sticky=ALL)
         
-        self.rowconfigure(0, weight=1)
+        self.grid(sticky=ALL)
+        self.rowconfigure(0, weight=1) 
+        #self.bind("<Button-1>", self.handler)
         Label(self, text="Frame {0}".format(1),bg="red").grid(row=0, column=0, sticky=ALL, columnspan=2)
+        
         self.rowconfigure(1, weight=1)
+        #self.bind("<Button-1>", self.handler)
         Label(self, text="Frame {0}".format(2),bg="green").grid(row=1, column=0, sticky=ALL, columnspan=2)
+        
+        #self.bind("<Button-1>", self.ignores)
         self.rowconfigure(5, weight=2)
         Label(self, text="Frame {0}".format(3),bg="yellow").grid(row=0, column=2, sticky=ALL, rowspan=2, columnspan=3)
-        self.rowconfigure(5, weight=1)
         
+        self.rowconfigure(5, weight=1)
         for c in range(5):
             self.columnconfigure(c, weight=1)
             Button(self, text="Col {0}".format(c)).grid(row=2, column=c, sticky=ALL)
+            
+    def handler(self, event):
+        print("clicked at", event.x, event.y)
+    
+    def ignores(self,event):
+        pass
+
 root = Tk()
-app = Application(master=root)                
+app = Application(master=root)   
+app.pack()             
 app.mainloop()
+
+
+
+#root = Tk()
+#def handler(event):
+#print("clicked at", event.x, event.y)
+#frame = Frame(root, width=100, height=100) frame.bind("<Button-1>", handler) frame.pack()
