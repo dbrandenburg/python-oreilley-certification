061873dc7b460e4afe4a1c9a442bdcb5022af3c2
Dennis Brandenburg <d.brandenburg@db-network.de>
2014-12-03 21:05:08 +0100

    Add table pop

---
+++ course2/lesson10/database.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+USERNAME = "dbranden"
+PASSWORD = "dennis"
+
+login_info = {
+            'host': "localhost",
+            'user': USERNAME,
+            'password': PASSWORD,
+            'database': USERNAME,
+            'port': 3306
+            }
