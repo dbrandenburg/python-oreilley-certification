b85e0c92ca992b23ed984194858c2d44c302109a
Dennis Brandenburg <d.brandenburg@db-network.de>
2015-01-07 15:37:11 +0100

    Attachment ifs to use correct variables, double named msg variable renamed. Body attached as MIMEtext

--- course2/lesson12/email.py
+++ course2/lesson12/email.py
@@ -4,25 +4,33 @@ email printer - A program to print an email based on passing a sender, a body an
 """
 import email, datetime
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email import encoders
+
 import mimetypes
 import os
 
 
 def print_email(to, body, *files):
+    
+    program_dir = os.path.dirname(__file__)
         
-    msg = email.message_from_string(body)
+    msg = MIMEMultipart()
     msg['From'] = 'sender@db-network.de'
     msg['To'] = to
     msg['Date'] = datetime.datetime.now().strftime("%d %b %Y %H:%M:%S -0600")
     msg['Subject'] = 'Email printer'
-    
-    print(msg.as_string())
-    print(files)
+    msg.attach(MIMEText(body, 'plain'))
     
     if not files:
         """Get all files in current directory"""
-        files = [f for f in os.listdir('.') if os.path.isfile(f)]
-
+        files = [program_dir + "/" + f for f in os.listdir(program_dir)] # if os.path.isfile(f)
+    
+    print(msg.as_string())
+    
     for fn in files:
         ctype, encoding = mimetypes.guess_type(fn)
         if ctype is None or encoding is not None:
@@ -30,30 +38,29 @@ def print_email(to, body, *files):
         maintype, subtype = ctype.split('/', 1)
         
         if maintype == 'text':
-            fp = open(path)
+            fp = open(fn)
             # Note: we should handle calculating the charset
-            msg = MIMEText(fp.read(), _subtype=subtype)
+            content = MIMEText(fp.read(), _subtype=subtype)
             fp.close()
         elif maintype == 'image':
-            fp = open(path, 'rb')
-            msg = MIMEImage(fp.read(), _subtype=subtype)
+            fp = open(fn, 'rb')
+            content = MIMEImage(fp.read(), _subtype=subtype)
             fp.close()
         elif maintype == 'audio':
-            fp = open(path, 'rb')
-            msg = MIMEAudio(fp.read(), _subtype=subtype)
+            fp = open(fn, 'rb')
+            content = MIMEAudio(fp.read(), _subtype=subtype)
             fp.close()
         else:
-            fp = open(path, 'rb')
-            msg = MIMEBase(maintype, subtype)
-            msg.set_payload(fp.read())
+            fp = open(fn, 'rb')
+            content = MIMEBase(maintype, subtype)
+            content.set_payload(fp.read())
             fp.close()
             # Encode the payload using Base64
-            encoders.encode_base64(msg)
+            encoders.encode_base64(content)
         # Set the filename parameter
-        #msg.add_header('Content-Disposition', 'attachment', filename=filename)
-        #outer.attach(msg)
-    # Now send or store the message
-    #scomposed = outer.as_string()
+        msg.add_header('Content-Disposition', 'attachment', filename=fn)
+        msg.attach(content)
+    print(msg.as_string())
         
     
 print_email("receiver@db-network.de","Email Body")
