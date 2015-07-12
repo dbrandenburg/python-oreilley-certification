f81ad4ad93101cbca40011401845595b6b07c1f9
Dennis Brandenburg <d.brandenburg@db-network.de>
2015-07-12 13:50:42 +0200

    Adder class comment cleanup.

--- course3/lesson1/adder.py
+++ course3/lesson1/adder.py
@@ -8,21 +8,3 @@ def object_adder(a, b):
 
 import sys
 print(sys.argv)
-
-"""Too complicated to have two functions, as adder() can be just three lines:
-
-def adder(a, b):
-    if either of the args is not an int:
-        raise TypeError
-    return a + b
-
-That's it.  Just replace the pseudocode with working Python in the first line.
-
-I am mainly interested in your assertRaises tests.  One does not call the callable in those, the arguments stay separate, like this:
-
-    self.assertRaises(TypeError, adder, 1.0, 2)  # should pass because 1.0 is a float.
-
-Give it another whirl!
-
-
--Kirby"""
