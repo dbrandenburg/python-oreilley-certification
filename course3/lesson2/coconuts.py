6cc07c8702cd0b1747864de3984e3c45a3ab48a8
Dennis Brandenburg <d.brandenburg@db-network.de>
2015-07-13 20:51:27 +0200

    Coconut lesson2 corrections

--- course3/lesson2/coconuts.py
+++ course3/lesson2/coconuts.py
@@ -1,37 +1,83 @@
 #!/usr/bin/env python3
 
-class Coconut():
+#class Coconut():
+#
+#    """A class to provide the weight of a coconut."""
+#
+#    def __init__(self, coconut_type = None):
+#        allowed_coconut_types = {'South Asian': 3,
+#                         'Middle Eastern': 2.5,
+#                         'American': 3.5}
+#        if not coconut_type:
+#            for k,v in allowed_coconut_types.items():
+#                setattr(self, k, v)
+#        elif coconut_type in allowed_coconut_types:
+#            setattr(self, 'coconut_type', coconut_type)
+#            setattr(self, 'coconut_weight', allowed_coconut_types[coconut_type])
+#        else:
+#            raise NameError(
+#                "API conflict: coconut_type '%s' is not valid" % coconut_type)
 
-    """A class to provide the weight of a coconut."""
+class Coconut:
+    pass
 
-    def __init__(self, coconut_type = None):
-        allowed_coconut_types = {'South Asian': 3,
-                         'Middle Eastern': 2.5,
-                         'American': 3.5}
-        if not coconut_type:
-            for k,v in allowed_coconut_types.items():
-                setattr(self, k, v)
-        elif coconut_type in allowed_coconut_types:
-            setattr(self, 'coconut_type', coconut_type)
-            setattr(self, 'coconut_weight', allowed_coconut_types[coconut_type])
-        else:
-            raise NameError(
-                "API conflict: coconut_type '%s' is not valid" % coconut_type)
+class SouthAsian(Coconut):
+    weight = 3
 
-            
+class MiddleEastern(Coconut):
+    weight = 2.5
+
+class American(Coconut):
+    weight = 3.5
+          
 class Inventory():
 
     """An Inventory class to manage coconuts"""
-
+    def __init__(self):
+        self.nuts = [ ]
+    
     def add_coconut(self, coconut):
-        "Adds a coconut for valid coconut types only"
+        """Adds a coconut for valid coconut types only"""
         if isinstance(coconut, str):
             raise AttributeError("Coconut must not be string")
-        if hasattr(self, 'Total'):
-            total_weight = self.__dict__['Total'] + coconut.coconut_weight
-            setattr(self, 'Total', total_weight)
-        else:
-            setattr(self, 'Total', coconut.coconut_weight)
+        self.nuts.append(coconut)
 
     def total_weight(self):
-        return self.__dict__['Total']
+        total = 0
+        for nut in self.nuts:
+            total = total + nut.weight
+        return total
+        
+        
+"""Have your Inventory class simply append coconut objects to self.nuts:
+
+class Inventory:
+
+    def __Init__(self):
+        self.nuts = [ ]
+
+such that add_coconut appends to self.nuts (nothing more, leave weights out of it completely),
+and total_weight simply loops over self.nuts adding the weight attributes of each nut.
+
+Here's a way you might want to organize the Coconut class hierarchy:
+
+class Coconut:
+    pass
+
+class SouthAsian(Coconut):
+    weight = 3
+
+class MiddleEastern(Coconut):
+    weight = 2.5
+
+class American(Coconut):
+    weight = 3.5
+
+Then simply going 
+
+nut = American()
+
+gets you a nut of the right weight, ready to add to inventory no problemo.
+
+
+-Kirby"""
--- course3/lesson2/test_coconuts.py
+++ course3/lesson2/test_coconuts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
-from coconuts import Coconut
+from coconuts import SouthAsian, MiddleEastern, American
 from coconuts import Inventory
 
 class TestCoconuts(unittest.TestCase):
@@ -9,25 +9,28 @@ class TestCoconuts(unittest.TestCase):
     
     def test_different_coconut_weights(self):
         """Tests whether coconuts types have different weights"""
-        allowed_coconuts = Coconut()
-        weights=allowed_coconuts.__dict__.values()
+        inventory = Inventory()
+        inventory.add_coconut(SouthAsian())
+        inventory.add_coconut(MiddleEastern())
+        inventory.add_coconut(American())
+        weights=[nut.weight for nut in inventory.nuts]
         self.assertEqual(len(weights),len(set(weights)))
         
     def test_datatype_errors(self):
         """Tests failing datatype validations"""
         inventory = Inventory()
         with self.assertRaises(AttributeError):
-            inventory.add_coconut('South Asian')
+            inventory.add_coconut('SouthAsian')
         
     def test_datatype_successes(self):
         """Tests succeeding datatype validations"""
         inventory = Inventory()
-        inventory.add_coconut(Coconut('South Asian'))
-        inventory.add_coconut(Coconut('South Asian'))
-        inventory.add_coconut(Coconut('Middle Eastern'))
-        inventory.add_coconut(Coconut('American'))
-        inventory.add_coconut(Coconut('American'))
-        inventory.add_coconut(Coconut('American'))
+        inventory.add_coconut(SouthAsian())
+        inventory.add_coconut(SouthAsian())
+        inventory.add_coconut(MiddleEastern())
+        inventory.add_coconut(American())
+        inventory.add_coconut(American())
+        inventory.add_coconut(American())
         self.assertEqual(19, inventory.total_weight(), 'Expect total weight to be 19')
         
 if __name__ == '__main__':
