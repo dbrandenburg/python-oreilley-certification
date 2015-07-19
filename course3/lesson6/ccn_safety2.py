73660586d70fe927aa06d7d783d978453ce1feb2
Dennis Brandenburg <d.brandenburg@db-network.de>
2015-07-19 18:51:43 +0200

    Lesson 6 regex corrections

--- course3/lesson6/ccn_safety2.py
+++ course3/lesson6/ccn_safety2.py
@@ -2,11 +2,16 @@
 
 import re
 
-def anonymize():
-    string = "Have you ever noticed, in television and movies, that phone numbers and credit cards are obviously fake numbers like 555-123-4567 or 5555-5555-5555-5555? It is because a number that appears to be real, such as 1234-5678-1234-5678, triggers the attention of privacy and security experts."
-    pattern=re.compile(r"""
+string = "Have you ever noticed, in television and movies, that phone numbers and credit cards are obviously fake" \
+    " numbers like 555-123-4567 or 5555-5555-5555-5555? It is because a number that appears to be real, such as" \
+    " 1234-5678-1234-5678, triggers the attention of privacy and security experts."
+
+
+def anonymize(string):
+
+    pattern = re.compile(r"""
         (\d{4}-)    #First four digits of a credit card
         {3}         #Each set repeated 3 times
         """, re.VERBOSE)
-    repl="XXXX-XXXX-XXXX-"
+    repl = "XXXX-XXXX-XXXX-"
     return pattern.sub(repl, string)
--- course3/lesson6/test_ccn_safety2.py
+++ course3/lesson6/test_ccn_safety2.py
@@ -6,7 +6,13 @@ import ccn_safety2
 class TestAnonymize(unittest.TestCase):
     
     def test_sample_text(self):
-       self.assertEqual("Have you ever noticed, in television and movies, that phone numbers and credit cards are obviously fake numbers like 555-123-4567 or XXXX-XXXX-XXXX-5555? It is because a number that appears to be real, such as XXXX-XXXX-XXXX-5678, triggers the attention of privacy and security experts.", ccn_safety2.anonymize(), 'Wrong start and end position returned')
+        test_string = "Have you ever noticed, in television and movies, that phone numbers and credit cards are obviously fake" \
+        " numbers like 555-123-4567 or 5555-5555-5555-5555? It is because a number that appears to be real, such as" \
+        " 1234-5678-1234-5678, triggers the attention of privacy and security experts."
+        self.assertEqual("Have you ever noticed, in television and movies, that phone numbers and credit cards are" \
+        " obviously fake numbers like 555-123-4567 or XXXX-XXXX-XXXX-5555? It is because a number that appears to be " \
+        "real, such as XXXX-XXXX-XXXX-5678, triggers the attention of privacy and security experts.", 
+       ccn_safety2.anonymize(test_string), 'Wrong start and end position returned')
     
 if __name__ == "__main__":
     unittest.main()
