4336fd85613f046a34003683c1c1fdb8e2948476
Dennis Brandenburg <dennisbrandenburg@Denniss-MacBook-Air.local>
2015-08-23 23:24:00 +0200

    Course 3 Lesson 13 done

---
+++ course3/lesson13/mathquiz.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import random
+from datetime import datetime, timedelta
+
+def random_summands():
+    a = random.randint(1,10)
+    b = random.randint(1,10)
+    return a,b
+
+def user_input(summand, second_summand):
+    input_text = "What is the sum of {0} and {1}?".format(summand, second_summand)
+    summ = input(input_text)
+    return summ
+
+def run_test(number_of_questions):
+    question = 1
+    question_results = []
+    total_time = 0
+
+    while question <= number_of_questions:
+        a,b = random_summands()
+        start_time = datetime.now()
+        summ = user_input(a, b)
+        stop_time = datetime.now()
+        time_taken = stop_time - start_time
+        total_time += time_taken.seconds
+        question += 1
+        if int(summ) == a + b:
+            print(summ + " is right!")
+            result = "right"
+        else:
+            print(summ + " is wrong!")
+            result = "wrong"
+        question_results.append([question,time_taken.seconds,result])
+    for item in question_results:
+        print("Question {0} took about {1} seconds to complete and was {2}.".format(item[0], item[1], item[2]))
+    print("You took {0} seconds to finish the quiz".format(total_time))
+    print("Your average time was {0} seconds per question".format(total_time / (question - 1)))
+
+run_test(5)
