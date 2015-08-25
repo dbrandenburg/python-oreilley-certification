8ebdc7aff244d81d88b405f8366b5353cbb85669
Dennis Brandenburg <dbranden@denniss-air-2.ad.here.com>
2015-08-25 11:16:21 +0200

    Mathquiz testing added

--- course3/lesson13/mathquiz.py
+++ course3/lesson13/mathquiz.py
@@ -12,6 +12,13 @@ def user_input(summand, second_summand):
     summ = input(input_text)
     return summ
 
+def check_result(summ,a,b):
+    if int(summ) == a + b:
+        return "right"
+    else:
+        return "wrong"
+
+
 def run_test(number_of_questions):
     question = 1
     question_results = []
@@ -25,16 +32,13 @@ def run_test(number_of_questions):
         time_taken = stop_time - start_time
         total_time += time_taken.seconds
         question += 1
-        if int(summ) == a + b:
-            print(summ + " is right!")
-            result = "right"
-        else:
-            print(summ + " is wrong!")
-            result = "wrong"
+        result = check_result(summ,a,b)
+        print(summ + " is " + result + "!")
         question_results.append([question,time_taken.seconds,result])
     for item in question_results:
         print("Question {0} took about {1} seconds to complete and was {2}.".format(item[0], item[1], item[2]))
     print("You took {0} seconds to finish the quiz".format(total_time))
     print("Your average time was {0} seconds per question".format(total_time / (question - 1)))
 
-run_test(5)
+if __name__ == "__main__":
+    run_test(5)
--- course3/lesson13/test_mathquiz.py
+++ course3/lesson13/test_mathquiz.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
-from unittest import unittest
+import unittest
+import mathquiz
+
+class TestAddresses(unittest.TestCase):
+    def test_check_sum(self):
+        self.assertEqual("right", mathquiz.check_result('5',2,3), '2 + 3 equals 5')
+        self.assertEqual('wrong', mathquiz.check_result('6',2,3), '2 + 3 should not equal 6')
+
+if __name__ == "__main__":
+    unittest.main()
