1002fa405dfb657e892856ef3aeec6dba7ac042a
Dennis Brandenburg <dbranden@Denniss-MacBook-Air-2.local>
2015-11-15 13:52:01 +0100

    Final lesson

--- course4/lesson10/cur/curdir.py
+++ course4/lesson10/cur/curdir.py
@@ -10,8 +10,10 @@ def pre_thread():
     texe = threading.Thread(target=dir_changing_thread)
     texe.start()
     texe.join()
-    pre_thread.current_directory = os.getcwd()
-    print("Stopped pre-thread in", pre_thread.current_directory)
+    pre_thread.current_directory_final = os.getcwd()
+    if pre_thread.current_directory != pre_thread.current_directory_final:
+        print("# Pre_thread stopped in different dir than it was started in #")
+    print("Stopped pre-thread in", pre_thread.current_directory_final)
 
 
 def dir_changing_thread():
@@ -22,13 +24,16 @@ def dir_changing_thread():
     print("Changed dir in dir-changing_thread to", current_directory)
     tpost = threading.Thread(target=post_thread)
     tpost.start()
+    tpost.join()
 
 
 def post_thread():
-    post_thread.current_directory = os.getcwd()
-    print("Started post-thread in", post_thread.current_directory)
-    print("Stopped post-thread")
+    current_directory = os.getcwd()
+    print("Started post-thread in", current_directory)
+    if current_directory != pre_thread.current_directory:
+        print("# Post_thread started in different directory than Pre_thread #")
+    print("Stopped post-thread in", current_directory)
+
 
 tpre = threading.Thread(target=pre_thread)
-tpre.start()
 tpre.start()
